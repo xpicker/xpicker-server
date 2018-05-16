457b854735eeb07a9e40ad8fd5d530b3a76e259f
LuGuoHuas <lu_guohua@163.com>
2018-05-16 11:01:08 +0800

    update: secret config update

--- src/main/secret_config.go
+++ src/main/secret_config.go
@@ -1,1 +1,11 @@
 package main
+
+//const PostgresqlUser = "xxx"
+//const PostgresqlPassword = "xxx"
+//const PostgresqlName = "xxx"
+//const PostgresqlPort = "xxx"
+//const RedisAddr = "xxx"
+//const RedisMaxIdle = xxx
+//const RedisMaxActive = xxx
+//const RedisIdleTimeout = xxx * time.Second
+//const RedisName = "xxx"
